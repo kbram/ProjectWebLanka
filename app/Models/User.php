7394735a8869948b8cfe72ace6b6ec90d6d21b19
Bavaram <nkbram95@gmail.com>
2023-05-20 18:23:43 +0530

    Add contact and home address fields in model

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'contact_number',
+        'home_address',
         'password',
     ];
 
