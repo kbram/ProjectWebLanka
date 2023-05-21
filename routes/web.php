534e482321fd67c815c858fd294943fc22e0d6b6
Bavaram <nkbram95@gmail.com>
2023-05-21 12:40:05 +0530

    Added routes for user CRUD functionality

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,9 +19,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [UserController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard/users/{id}/edit', [UserController::class, 'edit'])->middleware(['auth', 'verified'])->name('editUser');
+Route::post('/user/{id}/update', [UserController::class, 'update'])->middleware(['auth', 'verified'])->name('updateUser');
+Route::get('/dashboard/user/add', [UserController::class, 'create'])->middleware(['auth', 'verified'])->name('addUser');
+Route::post('/store-user', [UserController::class, 'store'])->middleware(['auth', 'verified'])->name('storeUser');
+Route::post('/user/delete/{id}', [UserController::class, 'destroy'])->middleware(['auth', 'verified'])->name('DeleteUser');
+Route::post('/users/filter/{email}', [UserController::class, 'searchUser'])->middleware(['auth', 'verified'])->name('searchUser');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
